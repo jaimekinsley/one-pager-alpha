a240e95338b9b18c65f1f5d3a5c768f27862c228
Jaime Sanders <jaimelyn517@gmail.com>
2020-11-02 12:00:01 -0800

    conditionally renders video and embeds link but there is a bug with Spotify

--- components/OnePagerVideo.tsx
+++ components/OnePagerVideo.tsx
@@ -13,12 +13,33 @@ export const OnePagerVideo = ({
   onePagerData,
   isLoading,
 }: OnePagerVideoProps) => {
+
+  if(!onePagerData.pitchVideoLink) {
+    return null;
+  }
+
+  const videoId = onePagerData.pitchVideoLink.substring(onePagerData.pitchVideoLink.lastIndexOf("?") + 3);
+
+  // remove this after Spotify works
+  console.log(videoId)
+
   return (
     <ContentCard title='Pitch Video' isLoading={isLoading}>
       <Heading as='h2' size='md' marginRight='10px'>
-        <a href={onePagerData.pitchVideoLink} target='_blank'>
+
+      <iframe
+        width="560"
+        height="315"
+        src={`https://www.youtube.com/embed/${videoId}`}
+        frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen>
+      </iframe>
+
+        {/* original link */}
+        {/* <a href={onePagerData.pitchVideoLink} target='_blank'>
           Link to Pitch Video
-        </a>
+        </a> */}
+
       </Heading>
     </ContentCard>
   );
