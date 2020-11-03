32c5bdc15c969b5d78355f4db3d8a613a1251ca8
Jaime Sanders <jaimelyn517@gmail.com>
2020-11-02 23:05:24 -0800

    creates new source code for youtube videos that includes time start

--- components/OnePagerVideo.tsx
+++ components/OnePagerVideo.tsx
@@ -18,17 +18,31 @@ export const OnePagerVideo = ({
     return null;
   }
 
-  const videoId = onePagerData.pitchVideoLink.substring(onePagerData.pitchVideoLink.lastIndexOf("?") + 3);
+  const paramsString = onePagerData.pitchVideoLink.split("?")[1];
+  const paramsArray = paramsString.split('&')
+  const paramsKeyValues = paramsArray.map((param) => param.split('='));
+  const paramsObj = paramsKeyValues.reduce((map, prop) => {
+    map[prop[0]] = prop[1];
+    return map;
+  }, {});
+  const videoId = paramsObj['v'];
+
+  let timeStart = 0;
+  if(paramsObj['t']){
+    timeStart = paramsObj['t'].slice(0, -1)
+  };
+
+
 
   // remove this after Spotify works
-  console.log(videoId)
+  console.log({paramsString, paramsArray, paramsKeyValues, paramsObj, videoId, timeStart})
 
   return (
     <ContentCard title='Pitch Video' isLoading={isLoading}>
       <iframe
         width="560"
         height="315"
-        src={`https://www.youtube.com/embed/${videoId}`}
+        src={`https://www.youtube.com/embed/${videoId}?start=${timeStart}`}
         frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen>
       </iframe>
