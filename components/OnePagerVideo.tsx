5366dd4db7625406d349fae08435319361bbe0fd
Jaime Sanders <jaimelyn517@gmail.com>
2020-11-02 23:13:59 -0800

    adds comment to pitchVideoLink parsing

--- components/OnePagerVideo.tsx
+++ components/OnePagerVideo.tsx
@@ -18,24 +18,20 @@ export const OnePagerVideo = ({
     return null;
   }
 
-  const paramsString = onePagerData.pitchVideoLink.split("?")[1];
-  const paramsArray = paramsString.split('&')
-  const paramsKeyValues = paramsArray.map((param) => param.split('='));
-  const paramsObj = paramsKeyValues.reduce((map, prop) => {
-    map[prop[0]] = prop[1];
-    return map;
-  }, {});
-  const videoId = paramsObj['v'];
+  // this parses the pitchVideoLink to account for time starts on Spotify video. In practice, it would be better to have all OnePage users submit the Embed link from YouTube that would not require any transformation.
+    const paramsString = onePagerData.pitchVideoLink.split("?")[1];
+    const paramsArray = paramsString.split('&')
+    const paramsKeyValues = paramsArray.map((param) => param.split('='));
+    const paramsObj = paramsKeyValues.reduce((map, prop) => {
+      map[prop[0]] = prop[1];
+      return map;
+    }, {});
+    const videoId = paramsObj['v'];
 
-  let timeStart = 0;
-  if(paramsObj['t']){
-    timeStart = paramsObj['t'].slice(0, -1)
-  };
-
-
-
-  // remove this after Spotify works
-  console.log({paramsString, paramsArray, paramsKeyValues, paramsObj, videoId, timeStart})
+    let timeStart = 0;
+    if(paramsObj['t']){
+      timeStart = paramsObj['t'].slice(0, -1)
+    };
 
   return (
     <ContentCard title='Pitch Video' isLoading={isLoading}>
