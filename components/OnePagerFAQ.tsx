df61478675b560c612a811d3d25d37a31ecd561a
Jaime Sanders <jaimelyn517@gmail.com>
2020-11-02 17:34:41 -0800

    adds accordion feature with dummy text

--- components/OnePagerFAQ.tsx
+++ components/OnePagerFAQ.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Heading } from '@chakra-ui/core';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionHeader,
+  AccordionPanel,
+  AccordionIcon,
+  Heading,
+  Box
+} from "@chakra-ui/core";
 
 import { OnePagerData } from '../model/model';
 import { ContentCard } from './ContentCard';
@@ -16,7 +24,37 @@ export const OnePagerFAQ = ({
 
   return (
     <ContentCard title='FAQs' isLoading={isLoading}>
-      <h3>testing</h3>
+      <Accordion allowToggle>
+  <AccordionItem>
+    <AccordionHeader>
+      <Box flex="1" textAlign="left">
+        Section 1 title
+      </Box>
+      <AccordionIcon />
+    </AccordionHeader>
+    <AccordionPanel pb={4}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </AccordionPanel>
+  </AccordionItem>
+
+  <AccordionItem>
+    <AccordionHeader>
+      <Box flex="1" textAlign="left">
+        Section 2 title
+      </Box>
+      <AccordionIcon />
+    </AccordionHeader>
+    <AccordionPanel pb={4}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </AccordionPanel>
+  </AccordionItem>
+</Accordion>
     </ContentCard>
   );
 };
